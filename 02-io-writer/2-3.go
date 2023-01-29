5c2ad264f14ff11663a68f7e491208d06c207336
hayazaki <toru.hayazaki@gmail.com>
2023-01-29 21:50:41 +0900

    2-io.Writer 続き

--- 02-io-writer/2-3.go
+++ 02-io-writer/2-3.go
@@ -16,13 +16,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		"Hello": "world",
 	}
 
+	// responseWriterのWrite実行時にgzip変換する
 	gzipWriter := gzip.NewWriter(w)
 	defer gzipWriter.Close()
 
+	// gzip->responseの実行と同タイミングでstdoutにも内容を出力
 	writer := io.MultiWriter(gzipWriter, os.Stdout)
 
+	// writer出力時に内容をjson変換
 	encoder := json.NewEncoder(writer)
 	encoder.SetIndent("", "  ")
+	// json変換実行と同時にwriterのWriteが走る
 	encoder.Encode(source)
 }
 
