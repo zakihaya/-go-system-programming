af55739505b1ac589f15ae2b66048854c3bb65c8
hayazaki <toru.hayazaki@gmail.com>
2023-04-23 17:14:14 +0900

    11-コマンドシェル101 シェルがコマンドを起動するまで ユーザー入力を受付

--- 11-command-shell-101/run-cmd/run-cmd.go
+++ 11-command-shell-101/run-cmd/run-cmd.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+
 	"github.com/peterh/liner"
 )
 
 func main() {
 	line := liner.NewLiner()
 	line.SetCtrlCAborts(true)
+	for {
+		if cmd, err := line.Prompt(" "); err == nil {
+			if cmd == "" {
+				continue
+			}
+			// ここでコマンドを処理する
+		} else if errors.Is(err, io.EOF) {
+			break
+		} else if err == liner.ErrPromptAborted {
+			log.Print("Aborted")
+			break
+		} else {
+			log.Print("Error reading line:", err)
+		}
+	}
+
+	fmt.Println("== end")
 }
