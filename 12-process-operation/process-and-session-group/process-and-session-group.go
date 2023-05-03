84294e7845840ce7a9598001fce11ce2fd1d042d
hayazaki <toru.hayazaki@gmail.com>
2023-05-03 18:56:53 +0900

    12-プロセスの役割とGo言語による操作 プロセスグループとセッショングループ

---
+++ 12-process-operation/process-and-session-group/process-and-session-group.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func main() {
+	sid, _ := syscall.Getsid(os.Getpid())
+	fmt.Fprintf(os.Stderr, "グループID: %d セッションID: %d\n", syscall.Getpgrp(), sid)
+}
