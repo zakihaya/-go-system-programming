7aa4083ad95b3bdfa9888158826d806832e1d684
hayazaki <toru.hayazaki@gmail.com>
2023-05-06 12:40:45 +0900

    12-プロセスの役割とGo言語による操作 シグナルを他のプロセスに送る

--- 13-signal/signal-handler/signal-handler.go
+++ 13-signal/signal-handler/signal-handler.go
@@ -3,16 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
 
 func main() {
+	fmt.Printf("プロセスID: %d\n", os.Getpid())
+
 	ctx := context.Background()
 	sigctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
-	toctx, cancel2 := context.WithTimeout(ctx, time.Second*3)
+	toctx, cancel2 := context.WithTimeout(ctx, time.Second*20)
 	defer cancel2()
 
 	select {
